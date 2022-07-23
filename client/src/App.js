b97af2306bdcf73ac76ad4697ba0048917183870
MarekD97 <mdorosz2@wp.pl>
2022-07-23 21:35:00 +0200

    Add a client-side SocketContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,5 @@
 import React from "react";
-import { io } from "socket.io-client";
 import WebCamera from "./WebCamera";
-
-const socket = io("localhost:8000");
-
-socket.on("hello", (arg) => {
-  console.log(arg);
-});
-
-socket.emit("howdy", "stranger");
 
 const App = () => {
   return (
---
+++ client/src/context/SocketContext.js
@@ -0,0 +1,112 @@
+import React, { createContext, useRef } from "react";
+
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
+
+const SocketContext = createContext();
+
+const socket = io("localhost:8000");
+
+const SocketContextProvider = ({ children }) => {
+  const [stream, setStream] = useState(null);
+  const [myId, setMyId] = useState("");
+
+  const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+
+  const [name, setName] = useState("");
+
+  const myCameraRef = useRef();
+  const userCameraRef = useRef();
+
+  const connectionRef = useRef();
+
+  useEffect(() => {
+    // get the current camera stream from the device
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+        myCameraRef.current.srcObject = currentStream;
+      });
+
+    // get my id
+    socket.on("me", (id) => setMyId(id));
+
+    // call to user
+    socket.on("callUser", ({ from, name, signal }) => {
+      setCall({ isReceivedCall: true, from, name, signal });
+    });
+  }, []);
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: call.from });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userCameraRef.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
+  const callUser = (id) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: myId,
+        name,
+      });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userCameraRef.current.srcObject = currentStream;
+    });
+
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  };
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+    window.location.reload();
+  };
+
+  return (
+    <SocketContext.Provider
+      value={{
+        call,
+        callAccepted,
+        myCameraRef,
+        userCameraRef,
+        stream,
+        name,
+        setName,
+        callEnded,
+        myId,
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export { SocketContext, SocketContextProvider };
