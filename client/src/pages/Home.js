6d9c71e5012fdf59da72269ee2b32ca9ce898a7f
MarekD97 <mdorosz2@wp.pl>
2022-07-31 23:22:12 +0200

    Update Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,87 +1,34 @@
 import React, { useContext, useEffect } from "react";
 
 import { SocketContext } from "../context/SocketContext";
-import VideoPlayer from "../components/VideoPlayer";
-import Controls from "../components/Controls";
+import { Link, useNavigate } from "react-router-dom";
 
-import Box from "@mui/material/Box";
-
-import CallUser from "./CallUser";
-import AnswerCall from "./AnswerCall";
-import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
-  const {
-    myCameraRef,
-    userCameraRef,
-    stream,
-    callUser,
-    myId,
-    answerCall,
-    setStream,
-    callAccepted,
-    callEnded,
-    call,
-  } = useContext(SocketContext);
+  const { createRoom, roomId } = useContext(SocketContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // get the current camera stream from the device
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((currentStream) => {
-        setStream(currentStream);
-        myCameraRef.current.srcObject = currentStream;
-      });
-  }, []);
+    if (roomId !== "") navigate(`/room/${roomId}`);
+  }, [roomId]);
+
+  const handleCreateRoom = () => createRoom();
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <div className={styles["container"]}>
       <Navbar />
-      {/* <Box
-        flex={1}
-        display="flex"
-        flexDirection="column"
-        backgroundColor="primary.dark"
-      >
-        <Box position="absolute" width="12em" right={0} padding="1em">
-          <VideoPlayer cameraRef={myCameraRef} muted rounded />
-        </Box>
-        <VideoPlayer cameraRef={userCameraRef} />
-        {!callAccepted && !call.isReceivedCall && (
-          <Box
-            position="absolute"
-            display="flex"
-            width="100%"
-            height="100vh"
-            top={0}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <CallUser />
-          </Box>
-        )}
-        {call.isReceivedCall && !callAccepted && (
-          <Box
-            position="absolute"
-            display="flex"
-            width="100%"
-            height="100vh"
-            top={0}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <AnswerCall />
-          </Box>
-        )}
-        {callAccepted && !callEnded && (
-          <Box position="absolute" bottom={0} width="100%">
-            <Controls />
-          </Box>
-        )}
-      </Box> */}
-      <div style={{ flex: 1 }}></div>
-      <Controls />
+      <div className={styles["content"]}>
+        <h1 className={styles["header"]}>Start Video Chat</h1>
+        <button className={styles["button"]} onClick={handleCreateRoom}>
+          Create a room
+        </button>
+        or
+        <Link className={styles["button"]} to="/room">
+          Join a room
+        </Link>
+      </div>
     </div>
   );
 };
